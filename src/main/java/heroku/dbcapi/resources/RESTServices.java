27d4f0b5d50d3b10517d726f9d4c1719c33e2f38
Jesper Joergensen <jesper@heroku.com>
2011-12-24 15:48:35 -0800

    a few commands are working

--- src/main/java/heroku/dbcapi/resources/RESTServices.java
+++ src/main/java/heroku/dbcapi/resources/RESTServices.java
@@ -3,6 +3,8 @@ package heroku.dbcapi.resources;
 import heroku.dbcapi.DatabaseLink;
 import heroku.dbcapi.Env;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,18 +13,23 @@ import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
 
 import com.force.api.ApiConfig;
 import com.force.api.ApiSession;
 import com.force.api.Auth;
+import com.force.api.DescribeGlobal;
+import com.force.api.DescribeSObject;
 import com.force.api.ForceApi;
 import com.force.api.QueryResult;
 import com.inamik.utils.SimpleTableFormatter;
 import com.inamik.utils.TableFormatter;
 
-@Path("/data")
+@Path("/data/{database}")
 public class RESTServices {
 
 	static ForceApi coredb = new ForceApi(new ApiConfig().setForceURL(Env.FORCE_COREDB_URL));
@@ -35,7 +42,7 @@ public class RESTServices {
 	@Path("/query")
 	@Produces("text/plain")
 	@SuppressWarnings("rawtypes")
-	public String query(@QueryParam("q") String q, @QueryParam("database") String database, @HeaderParam("Authorization") String apiKey) {
+	public String query(@QueryParam("q") String q, @PathParam("database") String database, @HeaderParam("Authorization") String apiKey) {
 		System.out.println("query: "+q);
 		ApiSession session = apiKeyToAccessToken.get(apiKey+":"+database);
 		if(session == null) {
@@ -68,6 +75,71 @@ public class RESTServices {
 			b.append(s+"\n");
 		}
 		return b.toString();
+	}
+
+	@GET
+	@Path("/sobjects/")
+	@Produces("text/plain")
+	public StreamingOutput describeGlobal(@PathParam("database") String database, @HeaderParam("Authorization") String apiKey) {
+		ApiSession session = apiKeyToAccessToken.get(apiKey+":"+database);
+		if(session == null) {
+			String refreshToken = coredb.query("SELECT token__c FROM database__c WHERE name='"+database+"' AND developer__r.apiKey__c='"+apiKey+"'", DatabaseLink.class).getRecords().get(0).getToken();
+			session = Auth.refreshOauthTokenFlow(CONFIG, refreshToken);
+			apiKeyToAccessToken.put(apiKey+":"+database, session);
+		}
+		ForceApi targetdb = new ForceApi(new ApiConfig(), session);
+		System.out.println("Getting info for "+database);
+		DescribeGlobal dg = targetdb.describeGlobal();
+		final TableFormatter tf = new SimpleTableFormatter(false).nextRow();
+		tf.nextCell().addLine("Name").addLine("----");
+		tf.nextCell().addLine("Label").addLine("-----");
+		for (DescribeSObject ds : dg.getSObjects()) {
+			tf.nextRow()
+				.nextCell().addLine(ds.getName()+"  ")
+				.nextCell().addLine(ds.getLabel());
+		}
+
+		return new StreamingOutput() {
+			public void write(OutputStream output) throws IOException, WebApplicationException {
+				for(String s: tf.getFormattedTable()) {
+					output.write((s+"\n").getBytes());
+				}
+			}
+		};
+		
+	}
+
+	@GET
+	@Path("/sobjects/{sobject}")
+	@Produces("text/plain")
+	public StreamingOutput describeSObject(@PathParam("database") String database, @PathParam("sobject") String sobject, @HeaderParam("Authorization") String apiKey) {
+		ApiSession session = apiKeyToAccessToken.get(apiKey+":"+database);
+		if(session == null) {
+			String refreshToken = coredb.query("SELECT token__c FROM database__c WHERE name='"+database+"' AND developer__r.apiKey__c='"+apiKey+"'", DatabaseLink.class).getRecords().get(0).getToken();
+			session = Auth.refreshOauthTokenFlow(CONFIG, refreshToken);
+			apiKeyToAccessToken.put(apiKey+":"+database, session);
+		}
+		ForceApi targetdb = new ForceApi(new ApiConfig(), session);
+		System.out.println("Getting info for "+database);
+		final DescribeSObject ds = targetdb.describeSObject(sobject);
+		final TableFormatter tf = new SimpleTableFormatter(false).nextRow();
+		tf.nextCell().addLine("Name").addLine("----");
+		tf.nextCell().addLine("Type").addLine("----");
+		for (DescribeSObject.Field f : ds.getFields()) {
+			tf.nextRow()
+				.nextCell().addLine(f.getName()+"  ")
+				.nextCell().addLine(f.getType());
+		}
+
+		return new StreamingOutput() {
+			public void write(OutputStream output) throws IOException, WebApplicationException {
+				output.write(new String("Fields for SObject: "+ds.getName()+"\n\n").getBytes());
+				for(String s: tf.getFormattedTable()) {
+					output.write((s+"\n").getBytes());
+				}
+			}
+		};
+		
 	}
 
 }
